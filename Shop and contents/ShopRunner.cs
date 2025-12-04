51c551fecf985d37fc928992936dd53258477e9a
David-Ammon-Student <daama6e@student.oaisd.org>
2025-12-04 19:00:10 +0000

    started shop keep working on the credit funder

---
+++ Shop and contents/ShopRunner.cs
@@ -0,0 +1,17 @@
+namespace storeRunner
+{
+    public class Shop
+    {
+
+        string userInput
+        private int _CurrentFunds = ;
+        public int Credits
+        {
+            get{return _CurrentCredit;}
+            set{ _CurrentCredit = value;}
+        }
+
+        public string mowerType {get; set;} = "Basic Mower";
+
+    }
+}
